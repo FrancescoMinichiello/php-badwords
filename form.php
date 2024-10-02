2b82c213b8f34360a0deb432aa7e2229a5a0666a
FrancescoMinichiello <fraps2278@gmail.com>
2024-10-02 16:11:40 +0200

    Aggiunto paragrafo e parola con php

--- form.php
+++ form.php
@@ -1,6 +1,24 @@
 <?php
+    //prendiamo il valore che si trova dentro a paragrafo
+    $paragrafo = $_GET["paragrafo"];
 
-
+    //prendiamo il valore che si trova dentro a parola
+    $parola = $_GET["parola"];
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <p>Hai scritto questo paragrafo: <?php echo $paragrafo; ?></p>
+    <p>Hai scritto questa parola: <?php echo $parola ?></p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-badwords</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-6">
-                <form>
+                <form action="./form.php" method="GET">
                   <div class="form-group">
                     <label for="paragrafo">Inserisci un paragrafo</label>
                     <input type="text" class="form-control my-2" id="paragrafo" aria-describedby="paragrafo" placeholder="Inserisci un paragrafo" name="paragrafo">
