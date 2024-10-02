f5a4e9df80d3bdd7d13f2b2142ad6a8e544d0bf7
FrancescoMinichiello <fraps2278@gmail.com>
2024-10-02 15:52:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-6">
+                <form>
+                  <div class="form-group">
+                    <label for="paragrafo">Inserisci un paragrafo</label>
+                    <input type="text" class="form-control my-2" id="paragrafo" aria-describedby="paragrafo" placeholder="Inserisci un paragrafo" name="paragrafo">
+                  </div>
+                  <div class="form-group">
+                    <label for="parola">Inserisci una parola</label>
+                    <input type="text" class="form-control my-2" id="parola" aria-describedby="parola" placeholder="Inserisci una parola" name="parola">
+                  </div>
+                  <button class="btn btn-primary">Invia</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
